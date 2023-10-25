7204eb718b917cf40c900a7b41439c68c72ced93
Monkey12589 <132232662+Monkey12589@users.noreply.github.com>
2023-10-26 00:01:54 +0200

    video

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function scrollWinD(){
-    window.scrollBy(0, 20000);
+    window.scrollBy(0, 950);
   }
+
+function play(){
+    var video = document.getElementById("vid");
+    var button = document.getElementById("play");
+    if (video.paused || video.ended) {
+      button.title = "Wstrzymaj";
+      button.innerHTML = "Wstrzymaj";
+      video.play();
+    }
+    else {
+      button.title = "Wznów";
+      button.innerHTML = "Wznów";
+      video.pause();
+    }
+}
